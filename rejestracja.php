db40f875f4243a8a2675bfef25b661c798edd979
Snopinio <jansnopkowski64@gmail.com>
2024-05-14 20:17:03 +0200

    formularz rejestracji z hash hasla poprawka

--- rejestracja.php
+++ rejestracja.php
@@ -1,40 +1,47 @@
+<?php
+require_once('class/User.class.php');
+
+if(isset($_REQUEST['email'])&& isset($_REQUEST['password'])) {
+    //wysłano formularz - przechwyć i obrób dane
+    $email = $_REQUEST['email'];
+    $password = $_REQUEST['password'];
+    //wywołujemy metodę klasy
+    //metody statyczne w PHP wywołuje się poprzez ::
+    $result = User::Register($email, $password);
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formularz rejestracji</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
 <div id="container">
-        <div class="row text-center mt-5">
-    <h1>Rejestracja</h1>
-    </div>
-    <div class="row">
-        <form action="rejestracja.php">
-            <div class="mb-3 col-4 offset-4">
-                <label class="from-label w-100" for="emailInput">Podaj Email:</label>
-                <input type="email" class="from-controll w-100" name="email" id="emailInput">
-            </div>
-            <div class="mb-3 col-4 offset-4">
-                <label class="form-label w-100" for="passwordInput"> Podaj Hasło:</label>
-                <input class="from-controll w-100" type="password" name="password" id="passwordInput">
-            </div>
-            <div class="mb-3 col-4 offset-4">
-                <label for="passwordRepeat" class="from-label w-100">Powtórz Hasło:</label>
-                <input class="from-controll w-100" type="passwordRepeat" name="passwordRepeat" id="passwordRepeatInput">
-            </div>
-            <div class="mb-3 col-4 offset-4">
-                <label class="from-label" for="checkbox">Potwierdz rejestracje</label>
-                <input type="checkbox" id="checkbox">
-            </div>
-            <div class="mb-3 col-4 offset-4">
-            <button type="sumbit" class="btn btn-primary w-100">Zajerestruj</button>
-            </div>
-        </form>
-    </div>
-    </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <h1 class="text-center mt-5 mb-5">Rejestracja</h1>
+        <div id="loginFrom" class="col-4 offset-4 mt-5">
+            <form action="rejestracja.php" method="post">
+                <label for="emailInput" class="from-label">Adres e-mail:</label>
+                <input type="email" class="from-controll mb-3" name="email" id="emailInput">
+                <label for="passwordInput" class="from-label">Haslo:</label>
+                <input type="password" class="from-controll mb-3" name="password" id="passwordInput">
+                <button type="submit" class="btn btn-primary w-100 mt-3">Zajerestruj</button>
+            </form>
+            <?php
+            if (isset($result)) {
+                if ($result) {
+                    echo "Udało się utworzyć konto";
+                } else {
+                    echo "Nie udało się utworzyć konta";
+                }
+            }
+
+            ?>
+
+        </div>
+</div>
 </body>
 </html>
