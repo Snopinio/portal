02d5026bdf348d4902beae467020f2752c4575d7
Snopinio <jansnopkowski64@gmail.com>
2024-05-21 18:58:25 +0200

    uzupelnienie/naprawienie plikow php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require_once('class/User.class.php');
+require_once('class/Profile.class.php');
 session_start();
 ?>
 <!DOCTYPE html>
@@ -8,6 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strona główna</title>
+    <link rel="stylesheet" href="styllogowania.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
@@ -17,11 +20,20 @@ session_start();
 <a href="login.php">
 <button class="btn btn-primary">Logowanie</button>
 </a>
-<a href="register.php">
+<a href="rejestracja.php">
 <button class="btn btn-primary">Rejestracja</button>
 </a>
 <a href="profile.php">
 <button class="btn btn-primary">Profil</button>
 </a>
+<br>
+<?php
+$profiles = profile::GetAll();
+foreach ($profiles as $profile){
+    echo '<img src="'.$profile->getProfilePhotoURL().'" style="height: 200px">';
+    echo $profile->getFullName();
+    echo "<br>";
+}
+?>
 </body>
 </html>
--- profile.php
+++ profile.php
@@ -5,18 +5,19 @@ session_start();
 
 if(isset($_REQUEST['profileID'])) {
     $profileID = $_REQUEST['profileID'];
-    $p = Profile::Get($profileID);
+    $p = profile::Get($profileID);
 } else {
     if(isset($_SESSION['user'])) {
         //jest zalogowany użytkownik - pokaż jego profil
         //załaduj profil zalogowanego użytkownika
-        $p = Profile::GetUserProfile($_SESSION['user']->GetID());
+        $p = profile::GetUserProfile($_SESSION['user']->GetID());
     } else {
         //pokaż domyślny profil
-        $p = Profile::Get(3);
+        $p = profile::Get(3);
     }
     
 }
+    
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -25,14 +26,19 @@ if(isset($_REQUEST['profileID'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strona główna</title>
+    <link rel="stylesheet" href="styllogowania.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 
 <body>
 <h1>Profila gostka</h1>
-Imię i nazwisko: <?php echo $p->getFullName(); ?>
-Zdjecie profilowe: <img src="<?php $p->getProfilePhotoURL();?>">
+Imię i nazwisko: <?php echo $p->getFullName(); ?><br>
+Zdjecie profilowe: <br>
+ <img src="<?php echo $p->getProfilePhotoURL();?>"> <br>
 
+<a href="index.php"> <br>
+<button class="btn btn-primary">Strona główna</button> 
+</a>
 </body>
 </html>
